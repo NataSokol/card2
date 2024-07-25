c27c979cbfb98c5ab4622c91a9196af093d56b8b
NataSokol <nata28051995@icloud.com>
2024-07-25 19:28:04 +0300

    db

---
+++ db/seeders/20240725154757-CategoriesSeed.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('', [{
+      title: 'Киновикториночка',
+      image: false
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
